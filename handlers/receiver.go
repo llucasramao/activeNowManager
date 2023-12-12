18d94ae7ac159f945fb0ea342cb9a6fee39deb20
Lucas Ramão <ext-lanascimento@bbts.com.br>
2023-12-12 19:18:59 -0300

    fix: ports struct bug fixed

--- handlers/receiver.go
+++ handlers/receiver.go
@@ -10,73 +10,64 @@ import (
 )
 
 func findOrCreatePort(db *gorm.DB, portNumber int) (*models.Port, error) {
-	var existingPort models.Port
-
-	result := db.Where("port = ?", portNumber).First(&existingPort)
-	if result.Error == nil {
-		return &existingPort, nil
-	}
-
-	newPort := models.Port{Port: portNumber}
-	if err := db.Create(&newPort).Error; err != nil {
+	var port models.Port
+	if err := db.Where("port = ?", portNumber).FirstOrCreate(&port, models.Port{Port: portNumber}).Error; err != nil {
 		return nil, err
 	}
-
-	return &newPort, nil
+	return &port, nil
 }
-func NewReceived(c *gin.Context) {
-	var received models.Received
 
-	if err := c.ShouldBindJSON(&received); err != nil {
+func findOrCreateApp(db *gorm.DB, appName, appVersion string) (*models.App, error) {
+	var app models.App
+	if err := db.Where("name = ? AND version = ?", appName, appVersion).FirstOrCreate(&app, models.App{Name: appName, Version: appVersion}).Error; err != nil {
+		return nil, err
+	}
+	return &app, nil
+}
+
+func NewReceived(c *gin.Context) {
+	var receivedData models.Received
+	if err := c.ShouldBindJSON(&receivedData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	// Verificar se o IP já existe
-	var existingReceived models.Received
-	result := database.DB.Preload("Ports").Where("ip = ?", received.Ip).First(&existingReceived)
-
-	if result.Error == nil {
-		// O IP já existe, atualizar o registro existente
-		existingReceived.Hostname = received.Hostname
-		existingReceived.Os = received.Os
-
-		// Excluir todas as portas associadas ao Received
-		if err := database.DB.Model(&existingReceived).Association("Ports").Clear(); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao excluir as portas associadas"})
-			return
-		}
-
-		// Verificar e criar/atualizar as portas associadas
-		var receivedPorts []models.Port
-
-		for _, portNumber := range received.Ports {
-			port, err := findOrCreatePort(database.DB, portNumber.Port)
-			if err != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar a porta"})
-				return
-			}
-			receivedPorts = append(receivedPorts, *port)
-		}
-
-		// Atualizar a estrutura Received com as portas associadas
-		existingReceived.Ports = receivedPorts
-
-		// Salvar as alterações no banco de dados
-		if err := database.DB.Save(&existingReceived).Error; err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao atualizar o Received"})
-			return
-		}
-		c.JSON(http.StatusOK, existingReceived)
-	} else {
-		// O IP não existe, criar um novo registro
-		// (Note que as portas associadas também serão criadas no processo)
-		if err := database.DB.Create(&received).Error; err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao criar o Received"})
-			return
-		}
-		c.JSON(http.StatusOK, received)
+	var received models.Received
+	err := database.DB.Where("ip = ?", receivedData.Ip).Preload("Ports").Preload("Apps").FirstOrCreate(&received, models.Received{Ip: receivedData.Ip}).Error
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar o Received"})
+		return
 	}
+
+	// Atualizando hostname e OS
+	received.Hostname = receivedData.Hostname
+	received.Os = receivedData.Os
+
+	// Associando Portas e Apps
+	for _, portData := range receivedData.Ports {
+		port, err := findOrCreatePort(database.DB, portData.Port)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar a porta"})
+			return
+		}
+		received.Ports = append(received.Ports, *port)
+	}
+
+	for _, appData := range receivedData.Apps {
+		app, err := findOrCreateApp(database.DB, appData.Name, appData.Version)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar o App"})
+			return
+		}
+		received.Apps = append(received.Apps, *app)
+	}
+
+	if err := database.DB.Save(&received).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao salvar o Received"})
+		return
+	}
+
+	c.JSON(http.StatusOK, received)
 }
 
 // func NewReceived(c *gin.Context) {
@@ -87,30 +78,48 @@ func NewReceived(c *gin.Context) {
 // 		return
 // 	}
 
-// 	var receivedPorts []models.Port
+// 	var existingReceived models.Received
+// 	result := database.DB.Preload("Ports").Where("ip = ?", received.Ip).First(&existingReceived)
 
-// 	for _, portNumber := range received.Ports {
-// 		port, err := findOrCreatePort(database.DB, portNumber.Port)
-// 		if err != nil {
-// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar a porta"})
+// 	if result.Error == nil {
+// 		existingReceived.Hostname = received.Hostname
+// 		existingReceived.Os = received.Os
+
+// 		if err := database.DB.Model(&existingReceived).Association("Ports").Clear(); err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao excluir as portas associadas"})
 // 			return
 // 		}
-// 		receivedPorts = append(receivedPorts, *port)
+
+// 		var receivedPorts []models.Port
+
+// 		for _, portNumber := range received.Ports {
+// 			port, err := findOrCreatePort(database.DB, portNumber.Port)
+// 			if err != nil {
+// 				c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao encontrar/criar a porta"})
+// 				return
+// 			}
+// 			receivedPorts = append(receivedPorts, *port)
+// 		}
+
+// 		existingReceived.Ports = receivedPorts
+
+// 		if err := database.DB.Save(&existingReceived).Error; err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao atualizar o Received"})
+// 			return
+// 		}
+// 		c.JSON(http.StatusOK, existingReceived)
+// 	} else {
+// 		if err := database.DB.Create(&received).Error; err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao criar o Received"})
+// 			return
+// 		}
+// 		c.JSON(http.StatusOK, received)
 // 	}
-
-// 	received.Ports = receivedPorts
-
-// 	if err := database.DB.Create(&received).Error; err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Erro ao criar o Received"})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, received)
 // }
 
 func ViewReciveds(c *gin.Context) {
 	var recibers []models.Received
-	database.DB.Preload("Ports").Find(&recibers)
+	database.DB.Preload("Ports").Preload("Apps").Find(&recibers)
 	c.JSON(200, gin.H{
 		"status":  "geted",
 		"content": recibers,
--- models/models.go
+++ models/models.go
@@ -8,10 +8,18 @@ type Port struct {
 	Receiveds []Received `gorm:"many2many:reciber_ports;"`
 }
 
+type App struct {
+	gorm.Model
+	Name      string
+	Version   string
+	Receiveds []Received `gorm:"many2many:reciber_apps;"`
+}
+
 type Received struct {
 	gorm.Model
 	Ip       string
 	Hostname string
 	Os       string
 	Ports    []Port `gorm:"many2many:reciber_ports;"`
+	Apps     []App  `gorm:"many2many:reciber_apps;"`
 }
